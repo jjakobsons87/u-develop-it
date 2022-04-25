c39b4b44aa67bbeebdc0abae0b191c0138125712
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-04-24 23:23:39 -0400

    update delete and create

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //  import express and mysql2
+const express = require("express");
 const mysql = require('mysql2');
-// const dotenv = require('dotenv');
-const express = require('express');
+const e = require('express');
+const inputCheck = require('./utils/inputCheck');
 
 // server local 
 const PORT = process.env.PORT ||3001;
@@ -56,24 +57,55 @@ app.get('/api/candidate/:id', (req, res) => {
 });
 
 // delete a candidate 
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 // Create a candidate 
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(
+        body, 
+        'first_name', 
+        'last_name', 
+        'industry_connected'
+    );
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// })
+    const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+        VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
 
 // handle user requests that are not supported (404 not found)
 app.use((req,res) => {
